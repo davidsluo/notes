226f97b6174084177b634f94a136ce432b960cda
David Luo <davidl2381@gmail.com>
2017-01-22 19:42:19 -0500

    Made Makefile, README, most of logic of shifty done.

---
+++ CSCI_1730/Labs/Lab1/README.md
@@ -0,0 +1,11 @@
+# README
+Author: David Luo
+Student ID: 811357331
+
+## How to Compile and Run
+Run `make` within this directory. Optionally run `make clean` first.
+
+To run, run `./shifty`.
+
+## Reflection
+*todo*
--- CSCI_1730/Labs/Lab1/shifty.cpp
+++ CSCI_1730/Labs/Lab1/shifty.cpp
@@ -1,3 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include <bitset>
+
+using namespace std;
+
+void setValue(unsigned int & var, unsigned int k, unsigned int i, unsigned int val) {
+  int comparator = 15 << (i * 4);
+  var = (var & ~comparator) + (val << i * 4);
+}
+
+unsigned int getValue(unsigned int var, unsigned int k, unsigned int i) {
+  int comparator = 15 << (i * 4);
+  return (var & comparator) >> (i * 4);
+}
+
 int main() {
-  return 0;
+  unsigned int var = 44391;
+  unsigned int value = 10;
+
+  cout << "before\t" << bitset<16>(var) << endl;
+  cout << "insert\t" << bitset<4>(value) << endl;
+  setValue(var, 4, 1, value);
+  cout << "after\t" << bitset<16>(var) << endl;
+
+  return EXIT_SUCCESS;
 }
