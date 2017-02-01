a25cf08c835394af75e4ca98da89934eb6d4eb7c
David Luo <davidl2381@gmail.com>
2017-01-31 20:11:23 -0500

    degrees

---
+++ CSCI_1730/Labs/Luo-David-Lab02/degrees.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+double convertToFahrenheit(double celcius) {
+	return celcius * 9.0 / 5.0 + 32.0;
+}
+
+int main() {
+	double temp;
+	cout << "Enter a temperature in Celcius:";	
+	cin >> temp;
+	cout << "Temperature in Fahrenheit: " << convertToFahrenheit(temp) 
+		<< endl;
+}
