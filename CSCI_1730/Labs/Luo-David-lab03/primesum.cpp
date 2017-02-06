aa87267418b1b97561ab43fcf2c7f824bd54e364
davidsluo <davidl2381@gmail.com>
2017-02-06 22:19:33 +0000

    primesum working and somewhat optimzed

--- CSCI_1730/Labs/Luo-David-lab03/primesum.cpp
+++ CSCI_1730/Labs/Luo-David-lab03/primesum.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <sstream>
+#include <cmath>
 
 using namespace std;
 
@@ -10,7 +12,7 @@ bool isPrime(unsigned int x) {
 	if (x % 2 == 0 && x != 2)
 		return false;
 
-	for (unsigned int i = 3; i < x; i+=2)
+	for (unsigned int i = 3; i < sqrt(x) + 1; i+=2)
 		if (x % i == 0)
 
 			return false;
@@ -18,13 +20,24 @@ bool isPrime(unsigned int x) {
 	return true;
 }
 
-int main() {
+int main(int argc, char* argv[]) {
 	
 	unsigned int input;
-	cout << "Enter an integer between 1 and 100,000 inclusive ";
-	cin >> input;
+    if (argc != 2){
+    	cout << "Enter an integer between 1 and 100,000 inclusive: ";
+    	cin >> input;
+    } else {
+        // This was mainly so I could time how fast this is.
+        stringstream convert(argv[1]);
+        convert >> input;
+    }
 
-	unsigned int primeSum = 0;
+    if (input < 1 || input > 100000) {
+        cout << "Input out of range (1-100,000)" << endl;
+        return EXIT_FAILURE;
+    }
+
+	unsigned long primeSum = 0;
 	unsigned int primeCount = 0;
 	
 	int i = 0;
@@ -39,4 +52,6 @@ int main() {
 	}
 
 	cout << primeSum << endl;
+
+    return EXIT_SUCCESS;
 }	
