d80839c32c21bcec7862638328c9ec23d145986c
David Luo <davidl2381@gmail.com>
2017-02-21 10:34:59 -0500

    project 1 started

---
+++ CSCI_1730/Projects/Project1/Matrix.cpp
@@ -0,0 +1,41 @@
+#include "Matrix.h"
+
+Matrix::Matrix(uint rows, uint cols) {
+
+	this->rows = rows;
+	this->cols = cols;
+
+	this->array = new double * [rows];
+	
+	for(uint i = 0; i < rows; i++) {
+		array[i] = new double [cols];
+	}	
+}
+
+Matrix::Matrix(const Matrix & m) {
+	Matrix::Matrix(m.numRows(), m.numCols);
+}
+
+Matrix::~Matrix() {
+	for (uint i = 0; i < rows; i++) {
+		delete [] array[i];
+	}
+
+	delete [] array;
+}
+
+const uint Matrix::numRows() const{
+	return rows;
+}
+
+const uint Matrix::numCols() const{
+	return cols;
+}
+
+double & at(uint row, uint col) {
+	return array[row][col];
+}
+
+const double & at(uint row, uint col) const{
+	return array[row][col];
+}
---
+++ CSCI_1730/Projects/Project1/Matrix.h
@@ -0,0 +1,112 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+typedef unsigned int uint;
+typedef initializer_list<initializer_list<double>> i_list;
+class Matrix {
+    private:
+        double ** array;
+        uint rows;
+        uint cols;
+    public:
+	/**
+	 * Create a matrix with `rows` rows and `cols` columns.
+	 * @param rows The number of rows
+	 * @param cols The number of columns
+	 */
+        Matrix(uint rows, uint cols);
+
+	/**
+	 * Create a matrix from a list.
+	 * @param i_list The list to turn into a matrix
+	 */
+        Matrix(const i_list & list);
+
+	/**
+	 * Copy a Matrix.
+	 * @param m The matrix to copy
+	 */
+        Matrix(const Matrix & m);
+
+	/**
+	 * Destructor.
+	 */
+        ~Matrix();
+
+	/**
+	 * Add a scalar to this matrix.
+	 * @param s Scalar to add to this matrix
+	 */
+        Matrix add(double s) const;
+
+	/**
+	 * Add another matrix to this matrix.
+	 * @param m The matrix to add to this matrix
+	 */
+        Matrix add(const Matrix & m) const;
+
+	/**
+	 * Subtract a scalar from this matrix.
+	 * @param s The scalar to subtract from this matrix
+	 */
+        Matrix subtract(double s) const;
+
+	/**
+	 * Subtract another matrix from this matrix.
+	 * @param m The matrix to subtract from this matrix
+	 */
+        Matrix subtract(const Matrix & m) const;
+
+	/**
+	 * Multiply this matrix by a scalar.
+	 * @param s The scalar to multiply this matrix
+	 */
+        Matrix multiply(double s) const;
+
+	/**
+	 * Multiply this matrix by another matrix. This matrix is the
+	 * first matrix in the multiplication operation.
+	 * @param m The matrix to multiply this matrix by
+	 */
+        Matrix multiply(const Matrix & m) const;
+
+	/**
+	 * Divide this matrix by a scalar.
+	 * @param s The scalar to divide this scalar by.
+	 */
+        Matrix divide(double s) const;
+
+	/**
+	 * Get the transpose of this matrix.
+	 * @return The transpose of this matrix.
+	 */
+        Matrix t() const;
+
+	/**
+	 * Get the number of rows.
+	 * @return The number of rows
+	 */
+        const uint numRows() const;
+
+	/**
+	 * Get the number of columns
+	 * @return The number of columns
+	 */
+        const uint numCols() const;
+
+	/**
+	 * Get/set element at `row`,`col`
+	 * @param row The row
+	 * @param col The column
+	 */
+        double & at(uint row, uint col);
+
+	/**
+	 * Get/set element at `row`,`col` when using a const object
+	 * @param row The row
+	 * @param col the column
+	 */
+        const double & at (uint row, uint col) const;
+}; // Matrix
+
+#endif
