9eea9cfccd4cf1f2a0848182f9b764312304db65
davidsluo <davidl2381@gmail.com>
2017-03-13 23:59:47 -0400

    replaced test with lorem ipsum; can now print first page with controls

--- CSCI_1730/Labs/Luo-David-lab07/lab07.cpp
+++ CSCI_1730/Labs/Luo-David-lab07/lab07.cpp
@@ -14,33 +14,58 @@ int main(int argc, char *argv[]) {
     ifstream infile(argv[1]);
     
     initscr();
-    noecho();
-    keypad(stdscr, TRUE);
+  //noecho();
+  //cbreak();
 
+    WINDOW *pad = newpad(LINES, COLS);
+
+    keypad(pad, TRUE);
+    //scrollok(pad, TRUE);
+    
+    //TODO: don't cut off in middle of word?
     char c;
     while (!infile.eof()){
         infile >> noskipws >> c;
-        addch(c);
+        pechochar(pad, c);
     }
 
-    int ch, x=0, y=0;
-    while ((ch = getch()) != KEY_ENTER) {
+    int ch;
+    int x=0, y=0;
+    int left_edge=0, right_edge=COLS - 1;
+    int top_edge=0, bottom_edge=LINES - 1;
+
+    prefresh(pad, y, 0,0,0, COLS, LINES);
+    while ((ch = wgetch(pad)) != 'q') {
         switch (ch) {
+            case 'j':
             case KEY_DOWN:
+                if (y + 1 > LINES)
+                    break;
                 y++;
                 break;
+            case 'k':
             case KEY_UP:
+                if (y - 1 < 0)
+                    break;
                 y--;
                 break;
-            case KEY_LEFT:
-                x--;
-                break;
-            case KEY_RIGHT:
-                x++;
-                break;
+          //case 'l':
+          //case KEY_RIGHT:
+          //    if (x + 1 > COLS)
+          //        break;
+          //    x++;
+          //    break;
+          //case 'h':
+          //case KEY_LEFT:
+          //    if (x - 1 < 0)
+          //        break;
+          //    x--;
+          //    break;
         }
-        mvwin(stdscr, y,x);
-        refresh();
+        //mvwin(pad, y,x);
+        //prefresh: pad pminrow pmincol sminrow smincol smaxrow smaxcol 
+        prefresh(pad, y, x, top_edge, left_edge, bottom_edge, right_edge);
+
     }
 
     endwin();
