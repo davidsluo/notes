06b63b6eab623a8b9bddc37a612ab5da4c397339
davidsluo <davidsluo@users.noreply.github.com>
2017-11-22 15:13:59 -0500

    Main with "shell".

---
+++ CSCI_2720/Luo_assignment4/src/Main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <fstream>
+
+#include "Sorting.cpp"
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+using std::fstream;
+using std::string;
+
+int main(int argc, char *argv[]) {
+
+
+    fstream fs;
+    if (argc > 1)
+        fs.open(argv[1], fstream::in);
+    else
+        exit(EXIT_FAILURE);
+
+    vector<int> data;
+
+    if (fs.is_open()) {
+        int input;
+        fs >> input;
+        while (!fs.eof()) {
+            data.push_back(input);
+            fs >> input;
+        }
+        fs.close();
+    }
+
+    string command;
+
+    while (command != "e") {
+        cout << "(i) insertion sort, (m) merge sort, (q) quick sort, (e) exit" << endl;
+        cout << "Enter a command: ";
+        cin >> command;
+
+        vector<int> sorted = data;
+
+        // Insertion Sort
+        if (command == "i") {
+            ulong comparisons = insertionSort(sorted);
+
+            cout << "Insertion Sort" << endl;
+            for (auto item : sorted)
+                cout << item << " ";
+            cout << endl;
+
+            cout << "Comparisons: " << comparisons << endl;
+        } else if (command == "m") {
+            ulong comparisons = mergeSort(sorted);
+
+            cout << "Merge Sort" << endl;
+            for (auto item : sorted)
+                cout << item << " ";
+            cout << endl;
+
+            cout << "Comparisons: " << comparisons << endl;
+        } else if (command == "q") {
+            ulong comparisons = quickSort(sorted);
+
+            cout << "Quick Sort" << endl;
+            for (auto item : sorted)
+                cout << item << " ";
+            cout << endl;
+
+            cout << "Comparisons: " << comparisons << endl;
+        } else if (command == "e")
+            break;
+        else
+            cout << "Command not recognized. Try again." << endl;
+    }
+}
