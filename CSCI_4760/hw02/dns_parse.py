bd4cf53dc2afd16337aa8ef74e7939dca0f498d0
davidsluo <davidsluo@users.noreply.github.com>
2018-02-18 13:07:23 -0500

    recursively resolves pointers; now works with all types of dns records.

--- CSCI_4760/hw02/dns_parse.py
+++ CSCI_4760/hw02/dns_parse.py
@@ -1,4 +1,3 @@
-import copy
 import sys
 from pprint import pprint
 
@@ -17,17 +16,8 @@ class BytesView:
         self.raw = raw
         self.index = 0
 
-    #     self._sub_index = 0
-    #
-    # @property
-    # def sub_index(self):
-    #     return self._sub_index
-    #
-    # @sub_index.setter
-    # def sub_index(self, sub_index: int):
-    #     bytes_add, sub_add = divmod(sub_index, 8)
-    #     self._sub_index += sub_add
-    #     self.index += bytes_add
+    def skip(self, amount: int):
+        self.index += amount
 
     def read_bytes(self, amount: int):
         """
@@ -40,16 +30,6 @@ class BytesView:
         self.index += amount
         return ret
 
-    # def read_bits(self, amount: int = 1):
-    #     mask = 0b1
-    #     for _ in range(amount):
-    #         mask <<= 1
-    #         mask |= 1
-    #
-    #     b = self.read_bytes(amount % 8 + 1)
-    #
-    #     return
-
     def read_int(self, byte_length: int, *, signed=False):
         """
         Reads a length of bytes interpreted as a big-endian integer.
@@ -60,42 +40,31 @@ class BytesView:
         """
         return int.from_bytes(self.read_bytes(byte_length), 'big', signed=signed)
 
-    def read_name(self, *, check_pointer=True):
+    def read_name(self):
         """
-        Reads a domain name. Handles pointers.
+        Reads a domain name. Recursively handles pointers.
 
         :return: the domain name read.
         """
 
-        def read(length):
-            """
-            Helper method to read labels.
-            """
-            # nonlocal self
-            ret = ''
-            while length > 0:
-                ret += self.read_bytes(length).decode()
-                ret += '.'
-                length = self.read_int(1)
-            return ret
-
-        ret = None
-        if check_pointer:
-            type = self.read_int(1)
+        ret = ''
+        type = self.read_int(1)
+        while type > 0:
             if type & 0xC0 != 0:
                 # is pointer
                 temp_index = ((type & 0x3F) << 4) | self.read_int(1)
                 current_index = self.index
                 self.index = temp_index
-                ret = self.read_name(check_pointer=False)
+                # recursively resolve pointers
+                ret += self.read_name()
                 self.index = current_index
+                return ret
             else:
                 # is label
                 read_len = type
-                ret = read(read_len)
-        else:
-            read_len = self.read_int(1)
-            ret = read(read_len)
+                ret += self.read_bytes(read_len).decode()
+                ret += '.'
+            type = self.read_int(1)
 
         return ret
 
@@ -107,15 +76,6 @@ class BytesView:
         :return: an IP address split into a tuple
         """
         return tuple(self.read_int(1) for _ in range(length))
-
-    def __getitem__(self, item):
-        """
-        Allows for indicing and slicing of the raw bytes.
-
-        :param item: an integer or slice.
-        :return:
-        """
-        return self.raw.__getitem__(item)
 
 
 with open(sys.argv[1], 'rb') as f:
@@ -136,29 +96,56 @@ flags = {
     'RCODE': (raw_flags & (0b1111 << 0)) >> 0
 }
 counts = {
-    'QD': view.read_int(2),
-    'AN': view.read_int(2),
-    'NS': view.read_int(2),
-    'AR': view.read_int(2)
+    'QD': view.read_int(2),  # question
+    'AN': view.read_int(2),  # answer
+    'NS': view.read_int(2),  # authority
+    'AR': view.read_int(2)  # additional
 }
 
 # questions
-qnames = [view.read_name(check_pointer=False) for _ in range(counts['QD'])]
-qtype = view.read_int(2)
-qclass = view.read_int(2)
-
-# answers
-answers = []
-for _ in range(counts['AN']):
-    answer = {
+questions = [
+    {
         'name': view.read_name(),
         'qtype': view.read_int(2),
-        'qclass': view.read_int(2),
-        'ttl': view.read_int(4),
-        'rdlength': view.read_int(2),
-        'rdata': view.read_address()
-    }
+        'qclass': view.read_int(2)
+    } for _ in range(counts['QD'])
+]
 
-    answers.append(answer)
+
+def read_response_record(number):
+    """
+    Reads a resource record
+    :param number: how many to read.
+    :return: a dictionary of resource record values.
+    """
+    records = []
+    for _ in range(number):
+        rr = {
+            'name': view.read_name(),
+            'qtype': view.read_int(2),
+            'qclass': view.read_int(2),
+            'ttl': view.read_int(4),
+            'rdlength': view.read_int(2),
+        }
+
+        # A record
+        if rr['qtype'] == 1:
+            rr['rdata'] = view.read_address()
+        # NS, CNAME respectively
+        elif rr['qtype'] in (2, 5):
+            rr['rdata'] = view.read_name()
+        # Everything else
+        else:
+            rr['rdata'] = 'Record type unsupported.'
+            view.skip(rr['rdlength'])
+            continue
+        records.append(rr)
+    return records
+
+
+# resource records
+answers = read_response_record(counts['AN'])
+auths = read_response_record(counts['NS'])
+addi = read_response_record(counts['AR'])
 
 pprint(locals())
