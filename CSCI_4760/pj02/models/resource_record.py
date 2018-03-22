62cfc2dea711fe781f37c2f1a079958f8fcf63b6
davidsluo <davidsluo@users.noreply.github.com>
2018-03-22 07:42:47 -0400

    subclassing ResourceRecord

--- CSCI_4760/pj02/models/resource_record.py
+++ CSCI_4760/pj02/models/resource_record.py
@@ -1,15 +1,17 @@
-from .enums import QType, QClass
+from abc import ABC, abstractmethod
+
+from .enums import QClass, QType
 from .util import int_to_bytes
 
 
-class ResourceRecord:
+class ResourceRecord(ABC):
     def __init__(self,
                  name: str,
                  qtype: QType,
                  qclass: QClass,
                  ttl: int,
                  rdlength: int,
-                 rdata: str):
+                 rdata: bytes):
         self.name = name
         self.qtype = qtype
         self.qclass = qclass
@@ -17,7 +19,8 @@ class ResourceRecord:
         self.rdlength = rdlength
         self.rdata = rdata
 
-    def __bytes__(self):
+    @abstractmethod
+    def __bytes__(self) -> bytes:
         ret = bytearray()
         split = self.name.split('.')
         for segment in split:
@@ -27,9 +30,97 @@ class ResourceRecord:
         ret.extend(bytes(self.qtype))
         ret.extend(bytes(self.qclass))
         ret.extend(int_to_bytes(self.ttl))
-        # ret.append()  # TODO: R Data, consider making this class abc, and subclassing different qtypes.
+        # ret.append()  # This is done in subclasses
 
-        return bytes(ret)
+        return ret
 
     def __str__(self):
-        return f'<ResourceRecord name={self.name} rdata={self.rdata}>'
+        return f'<{self.__class__.__name__} name={self.name} qtype={self.qtype.name} rdata={self.rdata}>'
+
+
+class A(ResourceRecord):
+    def __init__(self, name: str, qclass: QClass, ttl: int, rdlength: int, rdata: bytes):
+        super().__init__(name, QType.A, qclass, ttl, rdlength, rdata)
+
+    def __bytes__(self):
+        header = super(A, self).__bytes__()
+
+
+class NS(ResourceRecord):
+    def __init__(self, name: str, qclass: QClass, ttl: int, rdlength: int, rdata: bytes):
+        super().__init__(name, QType.NS, qclass, ttl, rdlength, rdata)
+
+    def __bytes__(self):
+        header = super(NS, self).__bytes__()
+
+
+class CNAME(ResourceRecord):
+    def __init__(self, name: str, qclass: QClass, ttl: int, rdlength: int, rdata: bytes):
+        super().__init__(name, QType.CNAME, qclass, ttl, rdlength, rdata)
+
+    def __bytes__(self):
+        header = super(CNAME, self).__bytes__()
+
+
+class SOA(ResourceRecord):
+    def __init__(self, name: str, qclass: QClass, ttl: int, rdlength: int, rdata: bytes):
+        super().__init__(name, QType.SOA, qclass, ttl, rdlength, rdata)
+
+    def __bytes__(self):
+        header = super(SOA, self).__bytes__()
+
+
+class WKS(ResourceRecord):
+    def __init__(self, name: str, qclass: QClass, ttl: int, rdlength: int, rdata: bytes):
+        super().__init__(name, QType.WKS, qclass, ttl, rdlength, rdata)
+
+    def __bytes__(self):
+        header = super(WKS, self).__bytes__()
+
+
+class PTR(ResourceRecord):
+    def __init__(self, name: str, qclass: QClass, ttl: int, rdlength: int, rdata: bytes):
+        super().__init__(name, QType.PTR, qclass, ttl, rdlength, rdata)
+
+    def __bytes__(self):
+        header = super(PTR, self).__bytes__()
+
+
+class HINFO(ResourceRecord):
+    def __init__(self, name: str, qclass: QClass, ttl: int, rdlength: int, rdata: bytes):
+        super().__init__(name, QType.HINFO, qclass, ttl, rdlength, rdata)
+
+    def __bytes__(self):
+        header = super(HINFO, self).__bytes__()
+
+
+class MINFO(ResourceRecord):
+    def __init__(self, name: str, qclass: QClass, ttl: int, rdlength: int, rdata: bytes):
+        super().__init__(name, QType.MINFO, qclass, ttl, rdlength, rdata)
+
+    def __bytes__(self):
+        header = super(MINFO, self).__bytes__()
+
+
+class MX(ResourceRecord):
+    def __init__(self, name: str, qclass: QClass, ttl: int, rdlength: int, rdata: bytes):
+        super().__init__(name, QType.MX, qclass, ttl, rdlength, rdata)
+
+    def __bytes__(self):
+        header = super(MX, self).__bytes__()
+
+
+class TXT(ResourceRecord):
+    def __init__(self, name: str, qclass: QClass, ttl: int, rdlength: int, rdata: bytes):
+        super().__init__(name, QType.TXT, qclass, ttl, rdlength, rdata)
+
+    def __bytes__(self):
+        header = super(TXT, self).__bytes__()
+
+
+class ANY(ResourceRecord):
+    def __init__(self, name: str, qclass: QClass, ttl: int, rdlength: int, rdata: bytes):
+        super().__init__(name, QType.ANY, qclass, ttl, rdlength, rdata)
+
+    def __bytes__(self):
+        header = super(ANY, self).__bytes__()
