b0b531b238fb72f0a7375fbd3861ca948414bb87
davidsluo <davidsluo@users.noreply.github.com>
2018-03-22 11:44:44 -0400

    working tcp

--- CSCI_4760/pj02/dns/client.py
+++ CSCI_4760/pj02/dns/client.py
@@ -3,12 +3,14 @@ import socket
 from dns.constructor import DNSConstructor
 from dns.parser import DNSParser
 from models.enums import QType
+from models.util import int_to_bytes
 
 
 class Client:
     def __init__(self, ip, *, port=53, protocol='UDP'):
         self.ip = ip
         self.port = port
+        self.protocol = protocol
         if protocol == 'UDP':
             self.conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         elif protocol == 'TCP':
@@ -17,12 +19,19 @@ class Client:
     def query(self, qname: str, qtype: QType):
         self.conn.connect((self.ip, self.port))
 
-        request = DNSConstructor.construct(qname, qtype)
-        self.conn.send(bytes(request))
+        request = bytes(DNSConstructor.construct(qname, qtype))
 
+        if self.protocol == 'TCP':
+            request = int_to_bytes(len(request)) + request
+
+        self.conn.send(request)
         raw_resp = self.conn.recv(1024)
-        response = DNSParser.parse(raw_resp)
 
         self.conn.close()
+
+        if self.protocol == 'TCP':
+            raw_resp = raw_resp[2:]
+
+        response = DNSParser.parse(raw_resp)
 
         return response
