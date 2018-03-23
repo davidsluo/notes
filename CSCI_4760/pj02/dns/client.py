a0b46011bee2319953f3a266e27d371b17a6da9c
davidsluo <davidsluo@users.noreply.github.com>
2018-03-23 04:29:06 -0400

    automatic retry

--- CSCI_4760/pj02/dns/client.py
+++ CSCI_4760/pj02/dns/client.py
@@ -1,4 +1,6 @@
 import socket
+import time
+from typing import Callable
 
 from dns.constructor import DNSConstructor
 from dns.parser import DNSParser
@@ -7,31 +9,46 @@ from models.util import int_to_bytes
 
 
 class Client:
-    def __init__(self, ip, *, port=53, protocol='UDP'):
+    def __init__(self, ip, *, port=53, protocol='UDP', timeout=5, retries=3,
+                 on_timeout: Callable[[int], None] = lambda x: None):
         self.ip = ip
         self.port = port
         self.protocol = protocol
+        self.timeout = timeout
+        self.retries = retries
+
         if protocol == 'UDP':
             self.conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         elif protocol == 'TCP':
             self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+        self.conn.settimeout(timeout)
+        self.on_timeout = on_timeout
+
     def query(self, qname: str, qtype: QType):
-        self.conn.connect((self.ip, self.port))
-
+        # Construct request
         request = bytes(DNSConstructor.construct(qname, qtype))
-
         if self.protocol == 'TCP':
             request = int_to_bytes(len(request)) + request
 
-        self.conn.send(request)
-        raw_resp = self.conn.recv(1024)
+        # query dns server, retrying if necessary, with exponential backoff.
+        for try_count in range(self.retries):
+            try:
+                self.conn.connect((self.ip, self.port))
+                self.conn.send(request)
+                raw_resp = self.conn.recv(1024)
+                self.conn.close()
+                break
+            except TimeoutError:
+                self.on_timeout(try_count + 1)
+                time.sleep(2 ** try_count)
+        else:
+            # max retry count exceeded.
+            return None
 
-        self.conn.close()
-
+        # parse response
         if self.protocol == 'TCP':
             raw_resp = raw_resp[2:]
-
         response = DNSParser.parse(raw_resp)
 
         return response
