73e2cfb3d81a7c73945ab63239d37acc8452676e
davidsluo <davidsluo@users.noreply.github.com>
2018-04-05 11:36:10 -0400

    Address is now a class instead of just a namedtuple

--- CSCI_4760/pj03/utils.py
+++ CSCI_4760/pj03/utils.py
@@ -1,7 +1,14 @@
 import socket
-from collections import namedtuple
 
-Address = namedtuple('Address', ['host', 'port'])
+
+class Address:
+    def __init__(self, host: str, port: int):
+        self.host = host
+        self.port = port
+
+    def __str__(self):
+        return f'{self.host}:{self.port}'
+
 
 
 class SocketWrapper:
