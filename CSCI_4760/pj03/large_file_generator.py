9278f04e3465f7fb4c0fea722209727d8428c9e6
davidsluo <davidsluo@users.noreply.github.com>
2018-04-23 00:42:53 -0400

    better large file gen

--- CSCI_4760/pj03/large_file_generator.py
+++ CSCI_4760/pj03/large_file_generator.py
@@ -1,22 +1,27 @@
 import argparse
+import platform
 import random
+import subprocess
 
 
-def generate(file, size: int):
-    with file as f:
-        curr_size = 0
-        while curr_size < size:
-            try:
-                data = random.randint(0, 1 << 4096)
-                b = data.to_bytes(4096, byteorder='big', signed=False)
-                curr_size += f.write(b)
-            except KeyboardInterrupt:
-                break
+def generate(file, size):
+    if platform.system() == 'Linux':
+        p = subprocess.Popen(['dd', 'if=/dev/zero', f'of={file}', f'bs={size}', 'count=1'])
+    else:
+        with open(file, 'wb') as f:
+            curr_size = 0
+            while curr_size < eval(size):
+                try:
+                    data = random.randint(0, 1 << 4096)
+                    b = data.to_bytes(4096, byteorder='big', signed=False)
+                    curr_size += f.write(b)
+                except KeyboardInterrupt:
+                    break
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('where', type=argparse.FileType('wb'))
-    parser.add_argument('size', type=eval)
+    parser.add_argument('where')
+    parser.add_argument('size')
     args = parser.parse_args()
     generate(args.where, args.size)
