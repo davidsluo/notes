e5c0a1bb126c79312a1b4ba3ec56f7ab002e12f9
davidsluo <davidsluo@users.noreply.github.com>
2018-04-23 00:44:02 -0400

    added option for static id for testing

--- CSCI_4760/pj03/ftserver.py
+++ CSCI_4760/pj03/ftserver.py
@@ -10,7 +10,8 @@ log = logging.getLogger('ftserver')
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('port', type=int)
+    parser.add_argument('--static-id', type=int)
     args = parser.parse_args()
 
-    server = Server(Address('', args.port))
+    server = Server(Address('', args.port), args.static_id)
     server.serve()
