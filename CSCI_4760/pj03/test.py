88f4b68e2d9dd3619456c2af723a5998e9bd994e
davidsluo <davidsluo@users.noreply.github.com>
2018-04-22 23:27:35 -0400

    absolute python path

--- CSCI_4760/pj03/test.py
+++ CSCI_4760/pj03/test.py
@@ -9,6 +9,7 @@ receiver = 'vcf4'
 sender = 'vcf5'
 home_dir = Path('/home/ugrads/dluo')
 project_path = home_dir / 'notes/CSCI_4760/pj03'
+python_path = Path('/usr/local/python/bin/python3')
 
 large_file = project_path / 'test_files/large_file.raw'
 if not large_file.is_file():
@@ -18,8 +19,7 @@ target_file = project_path / 'large_file.raw'
 server_proc = subprocess.Popen(
     f"ssh -t dluo@{server} "
     f"'cd {project_path.absolute()}; "
-    f"source {home_dir / '.bash_login'}; "
-    f"python3 ftserver.py {server_port}'".split()
+    f"{python_path} ftserver.py {server_port}'".split()
 )
 
 power_range = range(10, 11)
@@ -36,8 +36,7 @@ with open('results.csv', 'w') as f:
             receiver_proc = subprocess.Popen(
                 f"ssh -t dluo@{server} "
                 f"'cd {project_path.absolute()}; "
-                f"source {home_dir / '.bash_login'}; "
-                f"python3 ftclient.py --receive 1 --server {server}:{server_port}' -c {num_connections} -s {size} --log-level SCRIPT".split(),
+                f"{python_path} ftclient.py --receive 1 --server {server}:{server_port}' -c {num_connections} -s {size} --log-level SCRIPT".split(),
                 stdout=subprocess.PIPE
             )
 
@@ -46,8 +45,7 @@ with open('results.csv', 'w') as f:
             sender_proc = subprocess.Popen(
                 f"ssh dluo@{server} "
                 f"'cd {project_path.absolute()}; "
-                f"source {home_dir / '.bash_login'}; "
-                f"python3 ftclient.py --send {id} {large_file} --server {server}:{server_port}'".split(),
+                f"{python_path} ftclient.py --send {id} {large_file} --server {server}:{server_port}'".split(),
                 stdout=subprocess.PIPE
             )
 
