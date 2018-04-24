096a03c43bfdfd170dbac76b8d608d3d9e0c86c9
davidsluo <davidsluo@users.noreply.github.com>
2018-04-23 21:27:54 -0400

    better testing code

--- CSCI_4760/pj03/test_sender.py
+++ CSCI_4760/pj03/test_sender.py
@@ -6,9 +6,12 @@ from file_transfer.sender import SenderClient
 from file_transfer.utils import Address
 from large_file_generator import generate
 
-source_file = Path('test_files/large_file.raw')
-if not source_file.is_file():
-    generate(str(source_file), 1 << 30)
+SOURCE_FILE = Path('test_files/large_file.raw')
+SOURCE_FILE_SIZE = 1 << 30  # 1 GB
+SERVER_ADDRESS = Address('vcf3', 4000)
+
+if not SOURCE_FILE.is_file():
+    generate(str(SOURCE_FILE), SOURCE_FILE_SIZE)
 
 s = socket.socket()
 with open('conn_info', 'r') as f:
@@ -18,5 +21,5 @@ with open('conn_info', 'r') as f:
 while True:
     num_conns: int = int.from_bytes(s.recv(32), 'big', signed=False)
     time.sleep(1)
-    sender = SenderClient(Address('vcf3', 4000))
-    sender.send(5, str(source_file), connections=num_conns)
+    sender = SenderClient(SERVER_ADDRESS)
+    sender.send(5, str(SOURCE_FILE), connections=num_conns)
