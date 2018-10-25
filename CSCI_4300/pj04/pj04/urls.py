71280933e9e0d43647f32bb8752b0c9f79db9a21
David Luo <mail@dsluo.me>
2018-10-25 05:12:53 -0400

    pj04 on vm285

--- CSCI_4300/pj04/pj04/urls.py
+++ CSCI_4300/pj04/pj04/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('polls', include('polls.urls'))
+    path('pj04/', include([
+        path('admin/', admin.site.urls),
+        path('', include('polls.urls'))
+    ]))
 ]
